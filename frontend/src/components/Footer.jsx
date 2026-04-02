c1ff67380ef80ea54648d229a0af1251c07159ea
PriyankaShinde30 <priyankads3001@gmail.com>
2026-04-02 22:49:04 +0530

    Initialize frontend layout with reusable components

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,38 @@
+const Footer = () =>{
+    return(
+        <div>
+            <div>
+                <span>Store</span>
+            </div>
+            <div>
+                <ul>
+                    <h4>Quick Links</h4>
+                    <li>Home</li>
+                    <li>Shop</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <h4>Support</h4>
+                    <li>About Us</li>
+                    <li>Contact</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+            <div>
+                <h4>Follow Us</h4>
+                <span>FaceBook </span>
+                <span>Twitter </span>
+                <span>Instagram </span>
+                <span>Mail</span>
+            </div>
+            <hr></hr>
+            <div>
+                <span>2026 Store. All rights reserved.</span>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+const Navbar = () => {
+    return(
+        <div>
+            <span>Store</span>
+            <form>
+                <input></input>
+            </form>
+            <div>
+                <span>Home</span>
+                <span>Shop</span>
+                <span>Categories</span>
+            </div>
+            <div>
+                <span>Shopping Cart</span>
+            </div>
+            <div>
+                <button>Sign In</button>
+            </div>
+        </div>
+    ); 
+};
+
+export default Navbar;
